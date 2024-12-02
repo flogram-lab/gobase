a3dbac9e8b7bff2b724417cdfef7ebd166fc34ed
akansandra <5148375+akasandra@users.noreply.github.com>
2024-12-03 02:34:37 +0400

    Add LoadTLSCredentials()

--- tls.go
+++ tls.go
@@ -49,3 +49,41 @@ func LoadTLSCredentialsServer() ([]grpc.ServerOption, error) {
 
 	return []grpc.ServerOption{grpc.Creds(tlsCredentials)}, nil
 }
+
+
+func LoadTLSCredentials() (credentials.TransportCredentials, error) {
+
+	TLS_AUTHORITY := GetenvStr("TLS_AUTHORITY", "", false)
+
+	var (
+		serverCertFile   = path.Join(TLS_AUTHORITY, "server-cert.pem") // Mutual TLS ?? And should it be??
+		serverKeyFile    = path.Join(TLS_AUTHORITY, "server-key.pem")
+		clientCACertFile = path.Join(TLS_AUTHORITY, "ca-cert.pem")
+	)
+
+	// Load certificate of the CA who signed client's certificate
+	pemClientCA, err := ioutil.ReadFile(clientCACertFile)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemClientCA) {
+		return nil, errors.New("failed to add client CA's certificate")
+	}
+
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair(serverCertFile, serverKeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
